cd18171b8d48e8daba4b114196e8c77b7065bed9
Season <ssseasonnn@gmail.com>
2016-12-12 10:02:40 +0800

    commit code

--- rxdownload2/src/test/java/zlc/season/rxdownload2/GMTTest.java
+++ rxdownload2/src/test/java/zlc/season/rxdownload2/GMTTest.java
@@ -2,14 +2,7 @@ package zlc.season.rxdownload2;
 
 import org.junit.Test;
 
-import rx.Observable;
-import rx.Subscriber;
-import rx.Subscription;
-import rx.functions.Action1;
-import rx.schedulers.Schedulers;
 import zlc.season.rxdownload2.function.Utils;
-
-import static java.util.concurrent.TimeUnit.SECONDS;
 
 /**
  * Author: Season(ssseasonnn@gmail.com)
@@ -38,37 +31,4 @@ public class GMTTest {
         System.out.println(str.substring(str.lastIndexOf('/') + 1));
     }
 
-    @Test
-    public void subscriptionTest() throws Exception {
-        Subscription subscription = Observable.just(1)
-                .subscribeOn(Schedulers.io())
-                .delay(5, SECONDS)
-                .doOnNext(new Action1<Integer>() {
-                    @Override
-                    public void call(Integer integer) {
-                        throw new RuntimeException("Oops!");
-                    }
-                })
-                .observeOn(Schedulers.test())
-                .subscribe(new Subscriber<Integer>() {
-                    @Override
-                    public void onCompleted() {
-                        System.out.println("complete");
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        System.out.println("error");
-                    }
-
-                    @Override
-                    public void onNext(Integer integer) {
-                        System.out.println("integer:" + integer);
-                    }
-                });
-        boolean flag = subscription.isUnsubscribed();
-        System.out.println("flag:" + flag);
-        Thread.sleep(10000);
-        System.out.println("flag:" + flag);
-    }
 }

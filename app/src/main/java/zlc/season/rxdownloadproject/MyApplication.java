9e054c8efa45be5419a0adbd3aab4fb27f230887
Season <ssseasonnn@gmail.com>
2017-03-03 11:00:24 +0800

    commit code.

--- app/src/main/java/zlc/season/rxdownloadproject/MyApplication.java
+++ app/src/main/java/zlc/season/rxdownloadproject/MyApplication.java
@@ -2,7 +2,6 @@ package zlc.season.rxdownloadproject;
 
 import android.app.Application;
 
-import com.facebook.stetho.Stetho;
 
 /**
  * Author: Season(ssseasonnn@gmail.com)
@@ -13,6 +12,5 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Stetho.initializeWithDefaults(this);
     }
 }

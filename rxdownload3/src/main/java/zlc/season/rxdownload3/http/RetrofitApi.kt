54cfdd088881edc1c723218b2bb64ed6a76e8079
Season <ssseasonnn@gmail.com>
2017-08-17 23:53:04 +0800

    commit code.

--- rxdownload3/src/main/java/zlc/season/rxdownload3/http/RetrofitApi.kt
+++ rxdownload3/src/main/java/zlc/season/rxdownload3/http/RetrofitApi.kt
@@ -1,20 +1,17 @@
 package zlc.season.rxdownload3.http
 
 import io.reactivex.Maybe
-import io.reactivex.Observable
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.HEAD
+import retrofit2.http.Header
 import retrofit2.http.Url
 
 
 interface RetrofitApi {
 
-    @HEAD
-    fun checkUrlWithHEAD(@Url url: String): Maybe<Response<Void>>
-
     @GET
-    fun checkUrlWithGET(@Url url: String): Maybe<Response<Void>>
-
+    fun check(@Header("Range") range: String,
+              @Header("If-Modified-Since") lastModify: String,
+              @Url url: String): Maybe<Response<Void>>
 
 }

d2d5e24429aa6b65c85177f68cddeb2d1e075ce9
Season <ssseasonnn@gmail.com>
2018-04-12 10:24:10 +0800

    reformat code.

--- rxdownload3/src/main/java/zlc/season/rxdownload3/RxDownloadI.kt
+++ rxdownload3/src/main/java/zlc/season/rxdownload3/RxDownloadI.kt
@@ -9,27 +9,49 @@ import java.io.File
 
 
 interface RxDownloadI {
-	fun isExists(url: String): Maybe<Boolean>
-	fun isExists(mission: Mission): Maybe<Boolean>
-	fun create(url: String): Flowable<Status>
-	fun create(mission: Mission): Flowable<Status>
-	fun update(newMission: Mission): Maybe<Any>
-	fun start(url: String): Maybe<Any>
-	fun start(mission: Mission): Maybe<Any>
-	fun stop(url: String): Maybe<Any>
-	fun stop(mission: Mission): Maybe<Any>
-	fun delete(url: String, deleteFile: Boolean = false): Maybe<Any>
-	fun delete(mission: Mission, deleteFile: Boolean = false): Maybe<Any>
-	fun clear(url: String): Maybe<Any>
-	fun clear(mission: Mission): Maybe<Any>
-	fun getAllMission(): Maybe<List<Mission>>
-	fun createAll(missions: List<Mission>): Maybe<Any>
-	fun startAll(): Maybe<Any>
-	fun stopAll(): Maybe<Any>
-	fun deleteAll(deleteFile: Boolean = false): Maybe<Any>
-	fun clearAll(): Maybe<Any>
-	fun file(url: String): Maybe<File>
-	fun file(mission: Mission): Maybe<File>
-	fun extension(url: String, type: Class<out Extension>): Maybe<Any>
-	fun extension(mission: Mission, type: Class<out Extension>): Maybe<Any>
+    fun isExists(url: String): Maybe<Boolean>
+
+    fun isExists(mission: Mission): Maybe<Boolean>
+
+    fun create(url: String): Flowable<Status>
+
+    fun create(mission: Mission): Flowable<Status>
+
+    fun update(newMission: Mission): Maybe<Any>
+
+    fun start(url: String): Maybe<Any>
+
+    fun start(mission: Mission): Maybe<Any>
+
+    fun stop(url: String): Maybe<Any>
+
+    fun stop(mission: Mission): Maybe<Any>
+
+    fun delete(url: String, deleteFile: Boolean = false): Maybe<Any>
+
+    fun delete(mission: Mission, deleteFile: Boolean = false): Maybe<Any>
+
+    fun clear(url: String): Maybe<Any>
+
+    fun clear(mission: Mission): Maybe<Any>
+
+    fun getAllMission(): Maybe<List<Mission>>
+
+    fun createAll(missions: List<Mission>): Maybe<Any>
+
+    fun startAll(): Maybe<Any>
+
+    fun stopAll(): Maybe<Any>
+
+    fun deleteAll(deleteFile: Boolean = false): Maybe<Any>
+
+    fun clearAll(): Maybe<Any>
+
+    fun file(url: String): Maybe<File>
+
+    fun file(mission: Mission): Maybe<File>
+
+    fun extension(url: String, type: Class<out Extension>): Maybe<Any>
+
+    fun extension(mission: Mission, type: Class<out Extension>): Maybe<Any>
 }

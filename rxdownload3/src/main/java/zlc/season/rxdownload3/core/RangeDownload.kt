aa5086e2dcac4a4e7075d3d6349456714edaa5f8
deishelon <deishelon@gmail.com>
2019-01-26 15:40:00 +1300

    Clean up

--- rxdownload3/src/main/java/zlc/season/rxdownload3/core/RangeDownload.kt
+++ rxdownload3/src/main/java/zlc/season/rxdownload3/core/RangeDownload.kt
@@ -8,7 +8,6 @@ import zlc.season.rxdownload3.core.RangeTmpFile.Segment
 import zlc.season.rxdownload3.helper.logd
 import zlc.season.rxdownload3.http.HttpCore
 import java.io.File
-import java.util.concurrent.TimeUnit
 
 
 class RangeDownload(mission: RealMission) : DownloadType(mission) {
@@ -42,7 +41,7 @@ class RangeDownload(mission: RealMission) : DownloadType(mission) {
     }
 
     override fun download(): Flowable<out Status> {
-        if (!mission.actual.overwrite){
+        if (!mission.actual.overwrite) {
             if (isFinish()) {
                 return Flowable.empty()
             }

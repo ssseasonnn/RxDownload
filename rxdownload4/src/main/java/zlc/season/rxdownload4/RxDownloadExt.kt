8e5801d950a6a44088120c93c620c9a417285d05
Season <ssseasonnn@gmail.com>
2019-08-21 16:48:25 +0800

    commit code

---
+++ rxdownload4/src/main/java/zlc/season/rxdownload4/RxDownloadExt.kt
@@ -0,0 +1,68 @@
+package zlc.season.rxdownload4
+
+import io.reactivex.Flowable
+import zlc.season.rxdownload4.downloader.DefaultDispatcher
+import zlc.season.rxdownload4.downloader.Dispatcher
+import zlc.season.rxdownload4.request.Request
+import zlc.season.rxdownload4.request.RequestImpl
+import zlc.season.rxdownload4.storage.SimpleStorage
+import zlc.season.rxdownload4.storage.Storage
+import zlc.season.rxdownload4.task.SharedTask
+import zlc.season.rxdownload4.task.Task
+import zlc.season.rxdownload4.utils.safeDispose
+import zlc.season.rxdownload4.validator.SimpleValidator
+import zlc.season.rxdownload4.validator.Validator
+
+
+/**
+ * Returns a SharedTask represent the current task.
+ *
+ * A SharedTask means it can be subscribed in multiple places.
+ */
+fun Task.share(
+        header: Map<String, String> = RANGE_CHECK_HEADER,
+        maxConCurrency: Int = DEFAULT_MAX_CONCURRENCY,
+        rangeSize: Long = DEFAULT_RANGE_SIZE,
+        dispatcher: Dispatcher = DefaultDispatcher(),
+        validator: Validator = SimpleValidator(),
+        storage: Storage = SimpleStorage(),
+        request: Request = RequestImpl()
+): SharedTask {
+    val originFlowable = download(
+            header = header,
+            maxConCurrency = maxConCurrency,
+            rangeSize = rangeSize,
+            dispatcher = dispatcher,
+            validator = validator,
+            storage = storage,
+            request = request
+    )
+    val connectableFlowable = originFlowable.replay(1)
+
+    return SharedTask(connectableFlowable)
+}
+
+/**
+ * Start share download.
+ */
+fun SharedTask.start(): Flowable<Progress> {
+    if (!isStarted()) {
+        disposable = connectableFlowable.connect()
+    }
+    return connectableFlowable
+}
+
+/**
+ * Stop share download
+ */
+fun SharedTask.stop() {
+    disposable.safeDispose()
+}
+
+fun SharedTask.isStarted(): Boolean {
+    return disposable != null && !disposable!!.isDisposed
+}
+
+fun SharedTask.isStoped(): Boolean {
+    return disposable != null && disposable!!.isDisposed
+}
---
+++ rxdownload4/src/main/java/zlc/season/rxdownload4/task/SharedTask.kt
@@ -0,0 +1,10 @@
+package zlc.season.rxdownload4.task
+
+import io.reactivex.disposables.Disposable
+import io.reactivex.flowables.ConnectableFlowable
+import zlc.season.rxdownload4.Progress
+
+class SharedTask(
+        val connectableFlowable: ConnectableFlowable<Progress>,
+        var disposable: Disposable? = null
+)
